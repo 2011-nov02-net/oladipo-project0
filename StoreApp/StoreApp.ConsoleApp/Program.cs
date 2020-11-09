d4d4221a7fe4de539872ed29c152a02d20c7f9c7
Oladipo Osinubi <diposinubi@gmail.com>
2020-11-08 19:59:17 -0500

    inital commit

---
+++ StoreApp/StoreApp.ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security;
+using StoreApp;
+
+namespace StoreApp.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            GetProducts();
+        }
+
+        static List<Product> GetProducts(){
+            var numbers = new int[5];
+
+            numbers[3] = 1;
+            return null;
+        }
+    }
+}
---
+++ StoreApp/StoreApp.Library/Customer.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace StoreApp.Library
+{
+
+    public class Customer
+    {
+
+        //name
+        private string _firstName;
+        private string _lastName;
+
+
+       // set names to not allow empty string
+        public string FirstName {
+          get => _firstName; 
+          set {
+              if ( value.Length == 0){
+                   throw new ArgumentException("Name field must not be empty.");
+              }
+              _firstName = value;
+          }
+        }
+
+        public string LastName {
+            get => _lastName;
+            set {
+                if ( value.Length == 0){
+                   throw new ArgumentException("Name field must not be empty.");
+              }
+              _lastName = value;
+            }
+        }
+
+
+    public Customer ( string firstName, string lastName){
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    }
+}
