1ffe8a3c9cbecb5755dd3387b0e2f49aa89cea1b
Oladipo Osinubi <diposinubi@gmail.com>
2020-11-10 22:23:20 -0500

    made order library

--- StoreApp/StoreApp.Library/Order.cs
+++ StoreApp/StoreApp.Library/Order.cs
@@ -7,19 +7,26 @@ namespace StoreApp.Library
     class Order
     {
         //store location 
-        public string locationName{ get;}
+        public Store LocationName{ get;}
         
         //time ordered
         private DateTime Date {get;}
         //can contain a list of products
+
+        public Customer Customer {get;}
       
         //list of products 
-        private List<Product> products = new List<Product>();
+        private Dictionary<Product, int> Products;
 
 
 
-    //     //constructor
-    //     public Order ( string locationName )
+        //constructor
+        public Order ( Store name, Customer  cName, DateTime date, Dictionary<Product, int> products){
+            LocationName = name;
+            Customer = cName;
+            Date = date;
+            Products = new Dictionary<Product, int>(); 
+        }
 
 
      }
--- StoreApp/StoreApp.Tests/StoreLibTest.cs
+++ StoreApp/StoreApp.Tests/StoreLibTest.cs
@@ -1,13 +1,21 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
+using StoreApp.Library;
 
 namespace StoreApp.Tests
 {
     public class StoreLibTest
     {
         [Fact]
-        public void Test1()
+        public void addInvetory()
         {
+          string name = "coffee";
+          int quantity = 20;
+          double price = 2.99;
+          var product = new Product(name, price, quantity);
+          
+
 
         }
     }
